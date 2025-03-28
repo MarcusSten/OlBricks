23061a768d5e247c9d40628bf4978a819bc0a523
OlegSten <76691268+MarcusSten@users.noreply.github.com>
2025-03-28 12:00:44 -0400

    Update bricklink.js

--- api/bricklink.js
+++ api/bricklink.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default async function handler(req, res) {
   
-  import dotenv from 'dotenv';
+  require('dotenv').config();
   dotenv.config();
   
   const consumerKey = process.env.BRICKLINK_CONSUMER_KEY;
